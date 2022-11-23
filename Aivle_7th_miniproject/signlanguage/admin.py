a704baff42d86188f5e544c913cd00f69562b78e
simata98 <wlsrnr905@naver.com>
2022-11-23 20:22:22 +0900

    Merge pull request #2 from HeekunLim/heekun index.html 항목 추가 기능

--- Aivle_7th_miniproject/signlanguage/admin.py
+++ Aivle_7th_miniproject/signlanguage/admin.py
@@ -1,5 +1,47 @@
 from django.contrib import admin
+from .models import Result, AiModel
+from django.shortcuts import render
 
-from .models import Result
+import json
 
-admin.site.register(Result)
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models import Count
+from django.db.models.functions import TruncDay
+
+@admin.register(Result)
+class ResultAdmin(admin.ModelAdmin):
+  list_display = ('id', 'answer', 'result', 'pub_date')
+  ordering = ("-pub_date",)
+  
+  def changelist_view(self, request):
+    correct_array = [["is_it_correct", "yes"],
+                ["correct", 0],
+                ["incorrect", 0],]
+    for item in Result.objects.all():
+        if item.answer == item.result:
+            correct_array[1][1] += 1
+        else:
+            correct_array[2][1] += 1
+            
+    as_json = json.dumps(correct_array, cls=DjangoJSONEncoder)
+    
+    return super().changelist_view(request, {"correct_array": as_json})
+
+
+@admin.register(AiModel)
+class AimodelAdmin(admin.ModelAdmin):
+  list_display = ("id", "model_name", "uploader", "create_date")
+  ordering = ("-create_date",)
+  
+  def changelist_view(self, request, extra_context=None):
+    chart_data = (
+      AiModel.objects.annotate(date=TruncDay("create_date"))
+      .values("date")
+      .annotate(y=Count("id"))
+      .order_by("-date")
+    )
+    
+    as_json = json.dumps(list(chart_data), cls=DjangoJSONEncoder)
+    extra_context = extra_context or {"chart_data": as_json}
+    
+    return super().changelist_view(request, extra_context=extra_context)
--- Aivle_7th_miniproject/signlanguage/models.py
+++ Aivle_7th_miniproject/signlanguage/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,3 +9,12 @@ class Result(models.Model):
     answer = models.CharField(max_length=10)
     result = models.CharField(max_length=10)
     pub_date = models.DateTimeField('date published')
+
+class AiModel(models.Model):
+    model_name = models.CharField(max_length=30, null=True)
+    file_upload = models.FileField(upload_to='upload_model/', blank=True)
+    uploader = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    create_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f'[{self.pk}] {self.model_name} :: {self.uploader}'
